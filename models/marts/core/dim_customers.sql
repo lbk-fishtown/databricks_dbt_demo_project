b89946f0ff30972749df33a7eb2fedaf86959a6f
lee.bond.kennedy@dbtlabs.com <lee.bond.kennedy@dbtlabs.com>
2023-08-22 23:58:58 +0000

    added a customers table

---
+++ models/marts/core/dim_customers.sql
@@ -0,0 +1,34 @@
+{{
+    config(
+        tags=['finance', 'customer']
+    )
+}}
+
+with customer as (
+    select * from {{ ref('stg_tpch_customers') }}
+),
+nation as (
+    select * from {{ ref('stg_tpch_nations') }}
+),
+region as (
+    select * from {{ ref('stg_tpch_regions') }}
+)
+
+select 
+    customer.customer_key,
+    customer.name,
+    customer.address,
+    nation.name as nation,
+    region.name as region,
+    customer.phone_number,
+    customer.account_balance,
+    customer.market_segment
+from
+    customer
+    inner join nation
+        on customer.nation_key = nation.nation_key
+    inner join region
+        on nation.region_key = region.region_key
+
+order by
+    customer_key
